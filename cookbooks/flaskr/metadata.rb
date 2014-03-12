dc34523d7f63dabccc040fd38afa5818cc7f8eda
vagrant <vagrant@workstation>
2014-03-12 20:48:18 +0000

    run list and shit

--- cookbooks/flaskr/metadata.rb
+++ cookbooks/flaskr/metadata.rb
@@ -1,6 +1,6 @@
 name             'flaskr'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Rabbi Lopes Ltd.'
+maintainer_email 'asdjdasasdsd@asdjiasjasd.com'
 license          'All rights reserved'
 description      'Installs/Configures flaskr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
