7bfb6e62af32a986715005ad1a4e541414e784e1
Carlos Lopes <carlosaml@gmail.com>
2014-03-12 20:00:15 +0200

    a few more flaskr specs

--- cookbooks/flaskr/files/default/test/default_test.rb
+++ cookbooks/flaskr/files/default/test/default_test.rb
@@ -2,14 +2,24 @@ require 'minitest/spec'
 
 describe_recipe 'flaskr::default' do
   describe 'flaskr' do
-    it 'creates a directory structure in /var/flaskr/data' do
-      directory("/var/flaskr/data").must_exist.with(:owner, "vagrant")
+    describe 'directories' do
+      it 'for the database' do
+        directory("/var/flaskr/data").must_exist.with(:owner, "vagrant")
+      end
+      it 'for logs' do
+        directory("/var/log/flaskr").must_exist.with(:owner, "vagrant")
+      end
     end
-    it 'creates a file in /var/flaskr/data' do 
+  end
+  describe 'configuration file' do
+    it 'is created' do
       file("/var/flaskr/flaskr_config.py").must_exist.with(:owner, "vagrant")
     end
-    it 'populates the template with the correct value' do 
+    it 'contains the username' do
       file("/var/flaskr/flaskr_config.py").must_include node.flaskr.username
+    end
+    it 'contains the address to listen' do
+      file("/var/flaskr/flaskr_config.py").must_include node.flaskr.listen_address
     end
   end
 end
